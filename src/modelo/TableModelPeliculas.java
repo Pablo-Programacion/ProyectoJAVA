f38e7572591ff34e469bd484bd8ffb87b2b88f71
admin <admin@DAW-alumno-05>
2022-04-20 18:44:05 +0200

    Modelo

--- src/modelo/TableModelPeliculas.java
+++ src/modelo/TableModelPeliculas.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tema18;
+package modelo;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
