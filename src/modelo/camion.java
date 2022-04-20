abc3ec7d1f258a46ceb42257f1dd687047a41128
admin <admin@DAW-alumno-05>
2022-04-20 16:07:34 +0200

    Modelo

---
+++ src/modelo/camion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author admin
+ */
+public class camion {
+    int potencia;
+    String matricula;
+    String modelo;
+    String tipo;
+}
