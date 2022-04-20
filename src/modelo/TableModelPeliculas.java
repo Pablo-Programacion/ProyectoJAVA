4fdd55cbd183705c9092118c839eb0bedbab4fd6
PabloFernandezTAREAS <93275138+Pablo-Programacion@users.noreply.github.com>
2022-04-20 16:10:31 +0200

    prueba de texto

---
+++ src/modelo/TableModelPeliculas.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema18;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author MEDAC
+ */
+public class TableModelPeliculas extends AbstractTableModel {
+
+    private static final String[] columnNames = {"Titulo", "Año", "Puntuación", "Sinopsis"};
+    private final LinkedList<Pelicula> list;
+    private ConexionDB4O conn;
+
+    public TableModelPeliculas(ConexionDB4O conexion) {
+        list = new LinkedList<>();
+        conn = conexion;
+    }
+
+    public Pelicula getValueAt(int rowIndex) {
+        return list.get(rowIndex);
+    }
+
+    public void cargarPeliculas() throws SQLException {
+        // Obtiene la lista de peliculas de la BD
+        ArrayList<Pelicula> peliculas = conn.obtenerPeliculas();
+        System.out.println(peliculas.size());
+
+        // Borra el contenido anterior y añade el nuevo.
+        list.clear();
+        list.addAll(peliculas);
+
+        // Notifica a la vista que el contenido ha cambiado para que se refresque.
+        fireTableDataChanged();
+    }
+
+    public void insertar(String titulo, int año, int puntuacion, String sinopsis) throws SQLException {
+        /*** COMPLETAR CÓDIGO ***/
+        cargarPeliculas();
+    }
+
+    public void eliminar(String titulo) throws SQLException {
+        /*** COMPLETAR CÓDIGO ***/
+        cargarPeliculas();
+    }
+
+    public int actualizar(String tituloOriginal, String titulo, int año, int puntuacion, String sinopsis) throws SQLException {
+        int nfilas =0;
+        /*** COMPLETAR CÓDIGO ***/
+        cargarPeliculas();
+        return nfilas;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columnNames[column];
+    }
+
+    @Override
+    public int getRowCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return list.get(rowIndex).getTitulo();
+            case 1:
+                return list.get(rowIndex).getAño();
+            case 2:
+                return list.get(rowIndex).getPuntuacion();
+            case 3:
+                return list.get(rowIndex).getSinopsis();
+        }
+        return null;
+    }
+
+}
